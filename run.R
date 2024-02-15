57dde031f262313507461d6fb2ce15999a5f1aae
mcsiple-NOAA <margaret.siple@noaa.gov>
2024-02-14 17:21:04 -0800

    minor edits so that run.R creates the whole report

--- run.R
+++ run.R
@@ -2,13 +2,18 @@
 # Support scripts --------------------------------------------------------------
 
 source('./functions.R')
+source('./connect_to_oracle.R')
 source('./read.R')
 # source('./code/data_dl.R')
 source('./data.R')
 
+# Create output folder if you don't already have one
+dir.create(path = "./output")
+
+# Create the day's output folder
 dir_out <- paste0("./output/", Sys.Date())
 dir.create(path = dir_out)
-rmarkdown::render(paste0("./2023_ADFG_REPORT.Rmd"),
+rmarkdown::render(paste0("./ADFG_REPORT.Rmd"),
                   output_dir = dir_out,
-                  output_file = paste0("2023_ADFG_REPORT.docx"))
+                  output_file = paste0(maxyr,"_ADFG_REPORT.docx"))
 
