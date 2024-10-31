a7ff260079582f607c1b8d216f29faecf00ef29d
AlexandraDowlin-NOAA <Alexandra.Dowlin@noaa.gov>
2024-10-31 07:28:12 -0700

    2024 updates

--- run.R
+++ run.R
@@ -2,10 +2,35 @@
 # Support scripts --------------------------------------------------------------
 
 source('./functions.R')
+
+#skip connect and data_dl if data has already be recently downloaded
 source('./connect_to_oracle.R')
-source('./read.R')           #after initial download of data, hash tag this line out.
-# source('./code/data_dl.R')
-source('./data.R')
+source('./code/data_dl.R')
+
+# if data already downloaded then:
+catch0 <- read.csv("./data/oracle/racebase-catch.csv") %>% janitor::clean_names()
+haul0 <- read.csv("./data/oracle/racebase-haul.csv") %>% janitor::clean_names()
+specimen0 <- read.csv("./data/oracle/racebase-specimen.csv") %>% janitor::clean_names()
+species0 <- read.csv("./data/oracle/racebase-species.csv") %>% janitor::clean_names()
+stations_3nm0 <- read.csv("./data/oracle/ai-stations_3nm.csv") %>% janitor::clean_names()
+# stations_3nm0 <- read.csv("./data/oracle/goa-stations_3nm.csv") %>% janitor::clean_names() odd years only
+
+# --------------------------
+# Update based on survey and year 
+
+# SRVY <- "GOA"
+#cruise1 <- 202301
+
+SRVY <- "AI"
+cruise1 <- "202401"
+
+# -------------------
+source('./read.R') #creates tables needed for document
+source('./data.R') #anaylsis of data for document
+
+
+# -----------------------------
+#Create document
 
 # Create output folder if you don't already have one
 dir.create(path = "./output")
